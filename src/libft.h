6972fd634237599599b3699632ecb4f2082db4ef
ThomasSan <toms95@live.fr>
2015-09-07 18:54:23 +0200

    add du header, suppression du main.c

---
+++ src/libft.h
@@ -0,0 +1,11 @@
+#ifndef LIBFT_H
+#define LIBFT_H
+
+#include <unistd.h>
+#include <stdlib.h>
+
+void    ft_bzero(void *str, size_t n);
+void    *ft_memcpy(void *dest, const void *src, size_t n);
+void    *ft_memset(void *str, int c, size_t n);
+
+#endif
